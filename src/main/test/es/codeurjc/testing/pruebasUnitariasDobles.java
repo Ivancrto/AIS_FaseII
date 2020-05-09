dca15a525224f01476e5e5a9feed07111387cf7e
Ivancrto <ivancrtoc@gmail.com>
2020-05-09 13:58:23 +0200

    arreglos_

--- src/main/test/es/codeurjc/testing/pruebasUnitariasDobles.java
+++ src/main/test/es/codeurjc/testing/pruebasUnitariasDobles.java
@@ -1,14 +1,23 @@
-import static org.mockito.Mockito.mock;
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+package es.codeurjc.testing;
 
-import org.junit.jupiter.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import es.codeurjc.shop.domain.ShopException;
 import es.codeurjc.shop.domain.customer.Customer;
 import es.codeurjc.shop.domain.customer.CustomerCreditLimitExceededException;
+import es.codeurjc.shop.domain.customer.CustomerNotFoundException;
 import es.codeurjc.shop.domain.customer.CustomerService;
 import es.codeurjc.shop.domain.product.Product;
+import es.codeurjc.shop.domain.product.ProductNotFoundException;
 import es.codeurjc.shop.domain.product.ProductService;
 import es.codeurjc.shop.domain.purchase.PurchaseRepository;
 import es.codeurjc.shop.domain.purchase.PurchaseService;
@@ -21,6 +30,7 @@ dependencias de la clase: PurchaseRepository, CustomerService, ProductService y 
 public class pruebasUnitariasDobles {
 
 	PurchaseRepository purRep = mock(PurchaseRepository.class);
+	
 	CustomerService cService = mock(CustomerService.class);
 	ProductService pService = mock(ProductService.class);
 	NotificationService nService = mock(NotificationService.class);	
@@ -28,30 +38,31 @@ public class pruebasUnitariasDobles {
 	/*El pedido se realiza correctamente cuando el cliente tiene crédito y el producto stock.
 	Además, la notificación se envía.*/
 	@Test
-	public void PurchaseService_CreditoYProducto() {
+	public void PurchaseService_CreditoYProducto() throws ShopException {
 		Product p=pService.createProduct("Pan de Pipas",1,5);
 		Customer c= cService.createCustomer("Pipo", 100);
 		PurchaseService purService= new PurchaseService(purRep, cService, pService, nService);	
 		purService.createPurchase(c.getId(), p.getId());
 		
-		assertThat(pService.getProduct(p.getId()).getStock(),is(4));
-		assertThat(cService.getCustomer(c.getId()).getCredit(),is(99));
-		verify(nService,times(1)).nService.notify("Purchase: customer=" + c.getId() + ", product=" + c.getId());	//Comprobamos que se haya enviado la notificacion
+		assertThat("mensaje" ,pService.getProduct(p.getId()).getStock(), equalTo(4)); //is(4)
+		assertThat(cService.getCustomer(c.getId()).getCredit(),  equalTo(99)); //is(99)
+		verify(nService,times(1)).notify("Purchase: customer=" + c.getId() + ", product=" + c.getId());	//Comprobamos que se haya enviado la notificacion
 	
 	}
 	
 	/*El pedido no se realiza cuando el cliente no tiene crédito. Se verifica que la excepción es la
 	que corresponde a esta situación.*/
-	public void PurchaseService_NoCredito() {
+	public void PurchaseService_NoCredito() throws ProductNotFoundException, CustomerNotFoundException {
 		Product p=pService.createProduct("Lasagna",20,10);
 		Customer c= cService.createCustomer("Pepa", 3);
 		PurchaseService purService= new PurchaseService(purRep, cService, pService, nService);	
+
+		assertThrows(CustomerCreditLimitExceededException.class, () -> {purService.createPurchase(c.getId(), p.getId());});
 		
-		Assertions.assertThrows(CustomerCreditLimitExceededException.class, () -> {purService.createPurchase(c.getId(), p.getId());});
-		
-		assertThat(pService.getProduct(p.getId()).getStock(),is(10));
-		assertThat(cService.getCustomer(c.getId()).getCredit(),is(3));
-		verify(nService,times(0)).nService.notify("Purchase: customer=" + c.getId() + ", product=" + c.getId());	//Comprobamos que se haya enviado la notificacion
+		assertThat("mensaje", pService.getProduct(p.getId()).getStock(), equalTo(10));//is(10)
+		assertThat("mensaje", cService.getCustomer(c.getId()).getCredit(), equalTo(3));//is(3)
+		verify(nService, times(0)).notify("Purchase: customer=" + c.getId() + ", product=" + c.getId());
+		//nService.notify("Purchase: customer=" + c.getId() + ", product=" + c.getId());	//Comprobamos que se haya enviado la notificacion
 		
 		
 	}
