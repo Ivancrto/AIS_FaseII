f2e14199772de6eef0f65461deb7d59559e82881
Ivancrto <ivancrtoc@gmail.com>
2020-05-23 15:25:32 +0200

    fix

--- src/test/java/es/codeurjc/testing/PruebasSistemaApi.java
+++ src/test/java/es/codeurjc/testing/PruebasSistemaApi.java
@@ -1,5 +1,17 @@
 package es.codeurjc.testing;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.Test;
+
 public class PruebasSistemaApi {
 
+	@Test
+	public void test() {
+		
+		
+		
+	}
 }
